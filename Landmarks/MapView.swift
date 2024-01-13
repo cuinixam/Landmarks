5c465c18e88086eef888c2e90c5ae39ec10f965d
cuinixam <22344617+cuinixam@users.noreply.github.com>
2024-01-13 17:06:51 +0100

    add marker for the selected location

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -10,16 +10,30 @@ import MapKit
 
 struct MapView: View {
     var body: some View {
-        Map(initialPosition: .region(region))
+        Map(coordinateRegion: $region,
+            annotationItems: [place]
+        ){
+            somePlace in
+            MapMarker(coordinate: somePlace.coordinate, tint: .red)
+          }
     }
     
-    private var region: MKCoordinateRegion {
+    @State private var region =
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
             span: MKCoordinateSpan(latitudeDelta: 0.2
                                    , longitudeDelta: 0.2)
         )
-    }
+    
+    
+    struct Place: Identifiable {
+            let id = UUID()
+            let name: String
+            let coordinate: CLLocationCoordinate2D
+        }
+    
+   @State private var place = Place(name: "Turtle Rock", coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+
 }
 
 #Preview {
