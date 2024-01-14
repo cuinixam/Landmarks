56da2dc778a9e1cbc688be42555234b08295af6e
cuinixam <22344617+cuinixam@users.noreply.github.com>
2024-01-14 12:35:36 +0100

    add landmark assets and views

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,43 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Alexandru Maxiniuc on 14.01.24.
+//
+
+import Foundation
+// Needed for Image
+import SwiftUI
+// Needed for the map coordinates
+import CoreLocation
+
+// Addding Codable conformance makes it easier to move data between the structure and a data file
+// The original name is stored in a json file. We will rely on the Decodable component of the Codable
+// protocol to read data from the file
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    // we do not want to expose the imageName
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    // this property is private because it is only used to create a public computed property
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,43 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Alexandru Maxiniuc on 14.01.24.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+// This is a Swift function named "load".
+// It's designed to read and decode data from a file.
+// <T: Decodable> means it can work with any data type (generic), as long as that type conforms to the "Decodable" protocol.
+// The "Decodable" protocol allows data to be converted from its stored format, like JSON, into Swift objects.
+// "filename" is a parameter, which is a String that represents the name of the file we want to read.
+// Using the '_' in front of the argument name implies that this is not a named argument and one can
+// call the method like 'load("data.json")'
+// The "-> T" part indicates that the function will return a result of type "T" after decoding the data.
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    // guard is used for early exit from a scope (typically a function) when a certain condition is not met.
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
